b904c9cc5fcb2a466f5c380bf2b102a6fc193359
Bryan Baranosky <bryanbaranosky@gmail.com>
2026-04-13 20:42:39 -0400

    p1 functions

--- calculator.py
+++ calculator.py
@@ -5,7 +5,29 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    pass
+def add(a, b):
+    import math
+
+    def add(a, b):
+        return a + b
+
+    def subtract(a, b):
+        return a - b
+
+    def multiply(a, b):
+        return a * b
+
+    def divide(a, b):
+        if a == 0:
+            raise ZeroDivisionError
+        return b / a
+
+    def logarithm(a, b):
+        if a <= 0 or b <= 0:
+            raise ValueError
+        return math.log(b, a)
+
+    def exponent(a, b):
+        return a ** b
 
 
